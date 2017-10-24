1170a92cb825359b5d7ee1c67ea5b64f5fcf35e4
stremmekatrin <stremmekatrin@dv-schulen.de>
2017-10-24 08:34:32 +0200

    Update Form1.cs

--- WindowsFormsAppGithub/Form1.cs
+++ WindowsFormsAppGithub/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace WindowsFormsAppGithub
         public Form1()
         {
             InitializeComponent();
+            //Kommentar
         }
     }
 }
